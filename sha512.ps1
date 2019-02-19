a6a0efa15995f9d6f64c2bf0a695fc46ea96ec48
Simon Denier <simon@sogilis.com>
2019-02-19 15:18:25 +0100

    Be more verbose while running iterations

--- sha512.ps1
+++ sha512.ps1
@@ -6,4 +6,5 @@ Write-Output $File, $NbIter, $Output
 
 New-Item -ItemType directory -Force -Path $Output
 Get-FileHash $File -Algorithm SHA512 | Out-File -FilePath "$($Output)\sha512.txt"
-Measure-Command { 1..$NbIter | % { Get-FileHash $File -Algorithm SHA512 }} | Out-File -FilePath "$($Output)\time.txt"
+[Console]::Out.Flush()
+Measure-Command { 1..$NbIter | % { Write-Output "Iter $_" && Get-FileHash $File -Algorithm SHA512 }} | Out-File -FilePath "$($Output)\time.txt"
