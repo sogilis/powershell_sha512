0c2c08980dc5f7bba09629321eec1a52ef149828
Simon Denier <simon@sogilis.com>
2019-02-19 15:27:48 +0100

    More verbose and remove time.txt output

--- sha512.ps1
+++ sha512.ps1
@@ -2,9 +2,15 @@ $File = $Args[0]
 $NbIter = $Args[1]
 $Output = "c:\data\output"
 
-Write-Output $File, $NbIter, $Output
-
+Write-Output "Parameters $File, $NbIter, $Output"
 New-Item -ItemType directory -Force -Path $Output
+
+Write-Output "Computing SHA512"
+Get-FileHash $File -Algorithm SHA512 | Write-Output
 Get-FileHash $File -Algorithm SHA512 | Out-File -FilePath "$($Output)\sha512.txt"
+
 [Console]::Out.Flush()
-Measure-Command { 1..$NbIter | % { Write-Output "Iter $_" && Get-FileHash $File -Algorithm SHA512 }} | Out-File -FilePath "$($Output)\time.txt"
+
+Write-Output "Looping $NbIter x SHA512"
+Measure-Command { 1..$NbIter | % { Get-FileHash $File -Algorithm SHA512 }}
+Write-Output "Looping Done"
